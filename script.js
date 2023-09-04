b6e53c250c4ebcb41c2fec6a07ae2794112f02e0
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-09-04 08:59:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.addEventListener('load', time);
+
+function time() {
+	let date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+	let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+	let newDate = `${month}/${day}/${year}`;
+	const p = document.getElementById('timer');
+	p.textContent = `${newDate} , ${time}`;
+}
+ 
